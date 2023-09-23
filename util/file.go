0e62f8cf855066ed68d472230ac1f80101d832cd
Osama Khalid <osama@codinghazard.com>
2023-09-23 10:57:58 -0400

    FileConnector no longer exposes closer

--- util/file.go
+++ util/file.go
@@ -2,18 +2,32 @@ package util
 
 import (
 	"context"
+	"io"
 	"os"
 
 	"github.com/sirupsen/logrus"
 )
 
+type ReadWriteNopCloserWrapper struct {
+	io.Reader
+	io.Writer
+}
+
+func (r *ReadWriteNopCloserWrapper) Close() error {
+	return nil
+}
+
 func FileConnector(file *os.File) IOConnector {
 	return func(procCtx context.Context, logger *logrus.Entry) <-chan IOConnectorResult {
 		returnCh := make(chan IOConnectorResult, 1)
 		defer close(returnCh)
+
 		returnCh <- IOConnectorResult{
-			ReadWriteCloser: file,
-			Err:             nil,
+			ReadWriteCloser: &ReadWriteNopCloserWrapper{
+				Reader: file,
+				Writer: file,
+			},
+			Err: nil,
 		}
 		return returnCh
 	}
